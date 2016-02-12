4a5472071354a3fcc96df29ad4e3ad47602e6bb1
FuriousSlade <cvslulu@gmail.com>
2016-02-12 15:26:06 +0800

    update pelican-swain

--- theme/js/gototop.js
+++ theme/js/gototop.js
@@ -1,17 +1,14 @@
 $(function(){
-    var d_top=$('#d-top');
+    var d_top=$('#go-top');
     document.onscroll=function(){
         var scrTop=(document.body.scrollTop||document.documentElement.scrollTop);
+        var d ={queue: false, duration: 500};
         if(scrTop>500){
             d_top.show();
         }else{
             d_top.hide();
             }
-
-    $('#d-top-a').click(function(){
-    $("html,body").animate({scrollTop: 0},500);
-
-    this.blur();
-    return false;
+    $('#go-top-i').click(function(){
+    $("html,body").animate({scrollTop: '0px'},d);
 });
 }});
