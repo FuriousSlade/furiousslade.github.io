a313e1469bf9eabeed553a81ac440b65fcb60497
FuriousSlade <cvslulu@gmail.com>
2016-02-21 20:38:13 +0800

    update gototop.js

--- theme/js/gototop.js
+++ theme/js/gototop.js
@@ -2,13 +2,13 @@ $(function(){
     var d_top=$('#go-top');
     document.onscroll=function(){
         var scrTop=(document.body.scrollTop||document.documentElement.scrollTop);
-        var d ={queue: false, duration: 500};
+        var d ={duration: 500};
         if(scrTop>500){
             d_top.show();
         }else{
             d_top.hide();
             }
     $('#go-top-i').click(function(){
-    $("html,body").animate({scrollTop: '0px'},d);
+    $("html,body").animate({scrollTop: 0},d);
 });
 }});
